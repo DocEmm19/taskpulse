857e2b9bc00b573cf379e25dc028705335cc9cb1
DocEmm19 <drmalikpiyush@gmail.com>
2026-08-27 00:58:43 +0530

    Change GAURAV's user ID to a specific UUID
    Updated GAURAV's user ID placeholder in the SQL script.

--- schema_and_setup.sql
+++ schema_and_setup.sql
@@ -610,7 +610,7 @@ commit;
 
 do $$
 declare
-  gaurav_id uuid := 'gaurav.thapar@redcliffelabs.com'; -- replace with GAURAV's user id
+  gaurav_id uuid := '9dc4f4bb-aa6e-4972-8c8d-1dd5ac630879'; -- replace with GAURAV's user id
   abhay_id  uuid := 'd26ab05a-aeeb-4d44-9731-50d9ac944143'; -- replace with ABHAY's user id
 begin
   if gaurav_id = '00000000-0000-0000-0000-000000000000' or abhay_id = '00000000-0000-0000-0000-000000000000' then
